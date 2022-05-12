6995c8a5c9e320fc0ae68b1a07623a3eb8446a6d
benttotoya <benttotoya@gmail.com>
2022-05-12 10:54:55 +0900

    초기 커밋.

---
+++ UncleBros/UncleBros/View/ItemListView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UncleBros.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemListView : ContentView
+    {
+        public ItemListView()
+        {
+            pName.Text = "박동흠";
+            pName.Padding = new Thickness(3, 3, 3, 3);
+            InitializeComponent();
+        }
+
+        public string Name
+        {
+            get { return pName.Text; }
+            set { pName.Text = value; }
+        }
+    }
+}
